0f13e5f2fbc24d4f6bca8497a5c7f5c90b5e254f
Yongfeng LI <wliyongfeng@gmail.com>
2019-05-31 19:50:23 +0800

    Set chainx_ws_addr to .env

--- src/chainx.js
+++ src/chainx.js
@@ -1,3 +1,10 @@
+require("dotenv").config();
+
+if (!process.env.chainx_ws_addr) {
+  console.error("没有设置chainx_ws_addr");
+  process.exit(1);
+}
+
 const ChainX = require("chainx.js").default;
 const { WsProvider } = require("chainx.js");
 const provider = new WsProvider("ws://192.168.1.130:8087", false);
