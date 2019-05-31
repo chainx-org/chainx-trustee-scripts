9c3e941a48a4a2b12cbf310f9875dd7412063806
Yongfeng LI <wliyongfeng@gmail.com>
2019-05-31 11:13:49 +0800

    Add script to show withdrawal list

---
+++ src/show-withdrawal-list.js
@@ -0,0 +1,34 @@
+const ChainX = new require("chainx.js").default;
+const chainx = new ChainX("wss://w1.chainx.org/ws");
+require("console.table");
+
+async function showWithdrawalList() {
+  await chainx.isRpcReady();
+
+  const withdrawalList = await chainx.asset.getWithdrawalList(
+    "Bitcoin",
+    0,
+    100 // 适当增大以显示全部提现
+  );
+
+  const normalizedList = withdrawalList.data.map(withdrawal => {
+    const address = chainx.account.encodeAddress(withdrawal.accountid);
+    const balance = withdrawal.balance / Math.pow(10, 8);
+    const status = (withdrawal.status || {}).value;
+    return {
+      id: withdrawal.id,
+      ["balance(BTC)"]: balance,
+      address,
+      status,
+      memo: withdrawal.memo,
+      txid: withdrawal.txid
+    };
+  });
+
+  console.table(normalizedList);
+
+  chainx.provider.websocket.close();
+  process.exit(0);
+}
+
+showWithdrawalList();
